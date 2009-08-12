2d0333ff998deacdd75320d95a6dcb415a546334
Mislav Marohnić <mislav.marohnic@gmail.com>
2009-08-12 13:38:58 +0200

    cache the absolute images path for growl messages

--- lib/rspactor/growl.rb
+++ lib/rspactor/growl.rb
@@ -2,13 +2,15 @@ module RSpactor
   module Growl
     extend self
     
+    IMAGE_PATH = File.expand_path File.dirname(__FILE__) + "/../../images/%s.png"
+    
     def notify(title, msg, img, pri = 0)
       system("growlnotify -w -n rspactor --image #{img} -p #{pri} -m #{msg.inspect} #{title} &") 
     end
     
     # failed | pending | success
     def image_path(icon)
-      File.expand_path File.dirname(__FILE__) + "/../../images/#{icon}.png"
+      IMAGE_PATH % icon
     end
   end
 end
